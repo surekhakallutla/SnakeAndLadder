5182fb111204c3ee44592c7f283703bae640d1d7
surekhakallutla <surekhakallutla@gmail.com>
2022-01-12 11:48:49 +0530

    Rolling a Dice between 1 to 6

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -13,8 +13,48 @@ public class SnakeAndLadder{
 		
 		System.out.println("Player is starting at position is " +position);
 		
+		int DiceValue = rollDice();
+		
+		System.out.println("The Dice Value is "+DiceValue);
+		
+		Random r = new Random();
+	    int opt =r.nextInt(3);
+	    
+	    switch(opt)
+	    {
+	    
+	    case 0:
+	    	
+	    	position = position;
+	    	System.out.println("The Player is not Playing and is at position "+position);
+	    	break;
+	    	
+	    case 1:
+	    	
+	    	position = position + DiceValue;
+	    	System.out.println("The Player got Ladder and is at position "+position);
+	    	break;
+	    	
+	    default:
+	    	
+	    	position = position - DiceValue;
+	    	if (position  < 0)
+	    		position = 0;
+	    	System.out.println("The Player got Snake and is at position "+position);
+	    	
+	    }
+	   
 				
 	}
+	
+	public static int rollDice()
+	{
+	    Random r = new Random();
+	    int n=r.nextInt(6)+1;
+	    return (n);
+	   
+	}	
+	
 	
 	
 }
