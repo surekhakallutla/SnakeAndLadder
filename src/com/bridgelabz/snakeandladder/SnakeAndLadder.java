ec2890afcc88f7f8911575c97a9f8aa85d9b1caf
surekhakallutla <surekhakallutla@gmail.com>
2022-01-12 13:15:25 +0530

    Checking for Options whether player is Not Playing or got  Ladder or Snake

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -10,40 +10,49 @@ public class SnakeAndLadder{
 		System.out.println("Welcome to Snake And Ladder Program");
 		
 		int position = 0;
+		int currentposition = 0;
+		int count =0;
+//		System.out.println("Player is starting at position " +position);
 		
-		System.out.println("Player is starting at position is " +position);
 		
-		int DiceValue = rollDice();
 		
-		System.out.println("The Dice Value is "+DiceValue);
-		
-		Random r = new Random();
-	    int opt =r.nextInt(3);
 	    
-	    switch(opt)
-	    {
+			int DiceValue = rollDice();
+			
+			System.out.println("The Dice Value is "+DiceValue);
+			
+			Random r = new Random();
+		    int opt = r.nextInt(3);
+			
+	    	
 	    
-	    case 0:
-	    	
-	    	position = position;
-	    	System.out.println("The Player is not Playing and is at position "+position);
-	    	break;
-	    	
-	    case 1:
-	    	
-	    	position = position + DiceValue;
-	    	System.out.println("The Player got Ladder and is at position "+position);
-	    	break;
-	    	
-	    default:
-	    	
-	    	position = position - DiceValue;
-	    	if (position  < 0)
-	    		position = 0;
-	    	System.out.println("The Player got Snake and is at position "+position);
-	    	
-	    }
-	   
+		    switch(opt)
+		    {
+		    
+		    case 0:
+		    	
+		    	position = currentposition;
+		    	System.out.println("The Player is not Playing and is at position "+position);
+		    	break;
+		    	
+		    case 1:
+		    	
+		    	position = currentposition + DiceValue;
+		    	if(position > 100)
+					position = currentposition;
+		    	System.out.println("The Player got Ladder and is at position "+position);
+		    	break;
+		    	
+		    default:
+		    	
+		    	position = currentposition - DiceValue;
+		    	if (position  < 0)
+		    		position = 0;
+		    	System.out.println("The Player got Snake and is at position "+position);
+		    	
+		    }
+		    
+		   
 				
 	}
 	
