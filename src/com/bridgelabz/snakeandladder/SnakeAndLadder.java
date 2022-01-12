f04a1c18bda3f081689b7a4141ab0cdd39a34bce
surekhakallutla <surekhakallutla@gmail.com>
2022-01-12 11:17:00 +0530

    Added Player Position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,7 +6,13 @@ public class SnakeAndLadder{
 	
 	public static void main (String[] args)
 	{
-		System.out.println("Welcome to Snake and Ladder ");
+		
+		System.out.println("Welcome to Snake And Ladder Program");
+		
+		int position = 0;
+		
+		System.out.println("Player is starting at position is " +position);
+		
 				
 	}
 	
