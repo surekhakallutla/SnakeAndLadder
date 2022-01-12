5863dfa031fa95a22172b358123064f153ca393e
surekhakallutla <surekhakallutla@gmail.com>
2022-01-12 11:14:26 +0530

    Welcome Message

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeandladder;
+
+import java.util.Random;
+
+public class SnakeAndLadder{
+	
+	public static void main (String[] args)
+	{
+		System.out.println("Welcome to Snake and Ladder ");
+				
+	}
+	
+	
+}
