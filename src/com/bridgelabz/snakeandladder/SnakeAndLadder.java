f37e10a5620f9848ae2c7b33485790a3b12f4270
surekhakallutla <surekhakallutla@gmail.com>
2022-01-12 13:31:45 +0530

    If player Position Below 0 Starts from 0

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -12,47 +12,53 @@ public class SnakeAndLadder{
 		int position = 0;
 		int currentposition = 0;
 		int count =0;
-//		System.out.println("Player is starting at position " +position);
+		
+		System.out.println("Player is starting at position " +position);
 		
 		
-		
+	    	while(currentposition <= 100)
+	    	{
+	    		int DiceValue = rollDice();
+				
+				System.out.println("The Dice Value is "+DiceValue);
+				
+				Random r = new Random();
+			    int opt = r.nextInt(3);
 	    
-			int DiceValue = rollDice();
-			
-			System.out.println("The Dice Value is "+DiceValue);
-			
-			Random r = new Random();
-		    int opt = r.nextInt(3);
-			
-	    	
-	    
-		    switch(opt)
-		    {
+			    switch(opt)
+			    {
+			    
+			    case 0:
+			    	
+			    	position = currentposition;
+			    	System.out.println("The Player is not Playing and is at position "+position);
+			    	break;
+			    	
+			    case 1:
+			    	
+			    	position = currentposition + DiceValue;
+			    	
+			    	System.out.println("The Player got Ladder and is at position "+position);
+			    	break;
+			    	
+			    default:
+			    	
+			    	position = currentposition - DiceValue;
+			    	if (position  < 0)
+			    		position = 0;
+			    	System.out.println("The Player got Snake and is at position "+position);
+			    	
+			    }
 		    
-		    case 0:
-		    	
-		    	position = currentposition;
-		    	System.out.println("The Player is not Playing and is at position "+position);
-		    	break;
-		    	
-		    case 1:
-		    	
-		    	position = currentposition + DiceValue;
-		    	if(position > 100)
-					position = currentposition;
-		    	System.out.println("The Player got Ladder and is at position "+position);
-		    	break;
-		    	
-		    default:
-		    	
-		    	position = currentposition - DiceValue;
-		    	if (position  < 0)
-		    		position = 0;
-		    	System.out.println("The Player got Snake and is at position "+position);
-		    	
-		    }
+		    if(position < 100)
+				   count++;
+			   
+			   else
+				   break;
+			    
+			    currentposition=position;
 		    
-		   
+	    	}   
 				
 	}
 	
